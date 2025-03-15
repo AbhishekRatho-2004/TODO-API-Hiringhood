d2b5a9a99430cd7bded90a7ae7f96669614f74f3
AbhishekRatho-2004 <134370229+AbhishekRatho-2004@users.noreply.github.com>
2025-03-15 19:06:13 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,24 @@ const { authenticateToken } = require("../middleware/authMiddleware")
 const User = require("../models/User")
 const router = require("express").Router()
 
+router.get("/",async (req,res)=>{
+    try{
+        const users = await User.find()
+        if(users.length==0){
+            return res.status(200).json({
+                status:true,
+                message:"No Users are there in the database"
+            })
+        }
+        res.status(200).json({
+            status:true,
+            data:users
+        })
+    }catch(err){
+         res.status(404).json({message: "User not found"})
+    }
+})
+
 router.get("/:id",authenticateToken,async (req,res)=>{
     try{
         if(req.params.id!==req.user.id){
